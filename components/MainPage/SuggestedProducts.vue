e60c1f189f69b70c80b2376ee6ec422e85d4ebf2
Amin <amin@Amins-MacBook-Pro.local>
2023-09-19 16:29:29 +0330

    fixed main page message

--- components/MainPage/SuggestedProducts.vue
+++ components/MainPage/SuggestedProducts.vue
@@ -1,12 +1,14 @@
 <template>
     <div v-if="Object.keys(mainPageComponents).length>0">
-        <CardHolder v-for="component in mainPageComponents" :key="component[0].displayMessage" :products="component" :message="component[0].displayMessage"/>
+        <CardHolder v-for="component in mainPageComponents" :key="component[0].displayMessage" :products="component" :message="getWord(component[0].displayMessage)"/>
     </div>
 
 </template>
 <script setup>
 import mainPage from "../../assets/statics/mainpage.json"
-
+import { useFiltersStore } from '~/app.vue'
+const filtersStore = useFiltersStore()
+const { getWord } = filtersStore
 const mainPageComponents = ref({})
 mainPageComponents.value = mainPage
 
